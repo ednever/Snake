cede01fd111617bda5ff5181278cd372d5774b97
Эдгар Неверовский <edgarneverovskij@gmail.com>
2022-03-23 00:26:45 +0200

    New

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -72,4 +72,208 @@ namespace Snake
 			return x + ", " + y + ", " + sym;
 		}
 	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	enum Direction
+	{
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	class Figure
+	{
+		protected List<Point> pList;
+		//функция прорисовки точек в листе
+		public void Draw()
+		{
+			foreach (Point p in pList)
+			{
+				p.Draw();
+			}
+		}
+		//функция столкновения фигуры "змейка" сама с собой
+		internal bool IsHit(Figure figure)
+		{
+			foreach (var p in pList)
+			{
+				if (figure.IsHit(p))
+					return true;
+			}
+			return false;
+		}
+		//функция столкновения фигуры "змейка" с фигурой точка
+		private bool IsHit(Point point)
+		{
+			foreach (var p in pList)
+			{
+				if (p.IsHit(point))
+					return true;
+			}
+			return false;
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	class FoodCreator
+	{
+		int mapWidht;
+		int mapHeight;
+		char sym;
+
+		Random random = new Random();
+		public FoodCreator(int mapWidth, int mapHeight, char sym)
+		{
+			this.mapWidht = mapWidth;
+			this.mapHeight = mapHeight;
+			this.sym = sym;
+		}
+		//определение местоположения точки "еда" и перезапись точки
+		public Point CreateFood()
+		{
+			int x = random.Next(2, mapWidht - 2);
+			int y = random.Next(2, mapHeight - 2);
+			return new Point(x, y, sym);
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	class HorizontalLine : Figure
+	{
+		public HorizontalLine(int xLeft, int xRight, int y, char sym)
+		{
+			//добавление точек в лист с точками по координате x
+			pList = new List<Point>();
+			for (int x = xLeft; x <= xRight; x++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	class VerticalLine : Figure
+	{
+		public VerticalLine(int yUp, int yDown, int x, char sym)
+		{
+			//добавление точек в лист с точками по координате y
+			pList = new List<Point>();
+			for (int y = yUp; y <= yDown; y++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	class Snake : Figure
+	{
+		Direction direction;
+		public int points;
+		//структура фигуры "змейка"
+		public Snake(Point tail, int length, Direction _direction)
+		{
+			direction = _direction;
+			pList = new List<Point>();
+			for (int i = 0; i < length; i++)
+			{
+				Point p = new Point(tail);
+				p.Move(i, direction);
+				pList.Add(p);
+			}
+		}
+		//функция изменения направления хвоста и головы фигуры "змейка" 
+		public void Move()
+		{
+			Point tail = pList.First();
+			pList.Remove(tail);
+			Point head = GetNextPoint();
+			pList.Add(head);
+
+			tail.Clear();
+			head.Draw();
+		}
+		//функция изменения местоположения головы
+		public Point GetNextPoint()
+		{
+			Point head = pList.Last();
+			Point nextPoint = new Point(head);
+			nextPoint.Move(1, direction);
+			return nextPoint;
+		}
+		//функция столкновения головы с хвостом
+		public bool IsHitTail()
+		{
+			var head = pList.Last();
+			for (int i = 0; i < pList.Count - 2; i++)
+			{
+				if (head.IsHit(pList[i]))
+					return true;
+			}
+			return false;
+		}
+		//обозначение клавиш в системе
+		public void HandleKey(ConsoleKey key)
+		{
+			if (key == ConsoleKey.LeftArrow)
+				direction = Direction.LEFT;
+			else if (key == ConsoleKey.RightArrow)
+				direction = Direction.RIGHT;
+			else if (key == ConsoleKey.DownArrow)
+				direction = Direction.DOWN;
+			else if (key == ConsoleKey.UpArrow)
+				direction = Direction.UP;
+		}
+		//функция увеличения фигуры "змейка" при столкновении с фигурой "еда"
+		public bool Eat(Point food)
+		{
+			Point head = GetNextPoint();
+			if (head.IsHit(food))
+			{
+				food.sym = head.sym;
+				pList.Add(food);
+				points++;
+				return true;
+			}
+			else
+				return false;
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+	class Walls
+	{
+		List<Figure> wallList;
+		//создание стен
+		public Walls(int mapWidth, int mapHeight)
+		{
+			wallList = new List<Figure>();
+			HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
+			HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, '+');
+			VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '+');
+			VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
+
+			wallList.Add(upLine);
+			wallList.Add(downLine);
+			wallList.Add(leftLine);
+			wallList.Add(rightLine);
+		}
+		//если фигура сталкивается с стеной, то возвращает True
+		internal bool IsHit(Figure figure)
+		{
+			foreach (var wall in wallList)
+			{
+				if (wall.IsHit(figure))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		//Отрисовка рамочки
+		public void Draw()
+		{
+			foreach (var wall in wallList)
+			{
+				wall.Draw();
+			}
+		}
+	}
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,69 +13,126 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-			int score = 0;
-            Console.WriteLine("Kirjuta oma nimi >>> ");
-            string name = Console.ReadLine();
-			Walls walls = new Walls(80, 25);
-			walls.Draw();
+            
+            
 
-			//Прорисовка фигуры "змейка"			
-			Point p = new Point(4, 5, '&');
-			Console.ForegroundColor = ConsoleColor.Green;
-			Snake snake = new Snake(p, 4, Direction.RIGHT);
-			
-			snake.Draw();
+            Thread.Sleep(2000);
 
-			//создание точки "еда"
-			FoodCreator foodCreator = new FoodCreator(80, 25, '*');
-			Point food = foodCreator.CreateFood();
-			food.Draw();
+            string[] menuItems = new string[] { "Start", "Author", "Players", "Quit" };
+            Console.SetWindowSize(80, 25);
+            Console.WriteLine("SNAKE GAME \n");
+            int row = Console.CursorTop;
+            int col = Console.CursorLeft;
+            int index = 0;
+            while (true)
+            {
+                Console.SetCursorPosition(col, row);
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (i == index)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    Console.WriteLine(menuItems[i]);
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (index < menuItems.Length - 1)
+                            index++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (index > 0)
+                            index--;
+                        break;
+                    case ConsoleKey.Enter:
+                        switch (index)
+                        {
+                            case 0:
+                                int score = 0;
+                                Console.Write("Kirjuta oma nimi >>> ");
+                                string name = Console.ReadLine();
+                                Console.Clear();
 
-			//процесс игры
-			while (true)
-			{
-				//если фигура "стена" и фигура "хвост змеи" касается фигуры "змейка" игра прекращается
-				if (walls.IsHit(snake) || snake.IsHitTail())
-				{
-					break;
-				}
-				//если змея ест еду очки увеличиваются
-				if (snake.Eat(food))
-				{
-					food = foodCreator.CreateFood();
-					food.Draw();
-					score++;
-				}
-				//если события не происходят, то фигура "змейка" движется
-				else
-				{
-					snake.Move();
-				}
 
-				Thread.Sleep(100);
-				if (Console.KeyAvailable)
-				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey(key.Key);
-				}
-			}
-			WriteGameOver(score, name);
+                                Walls walls = new Walls(80, 25);
+                                walls.Draw();
 
-			//работа с файлом
-            //string[] v = new string[5];
-            //v[0] = name;
-            //v[1] = "ZXC";
-            //StreamWriter to_file = new StreamWriter("Gamers.txt", true);
-            //for (int i = 0; i < v.Length; i++)
-            //{
-            //    to_file.WriteLine(v[i]);
-            //}
+                                //Прорисовка фигуры "змейка"			
+                                Point p = new Point(4, 5, '&');
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Snake snake = new Snake(p, 4, Direction.RIGHT);
 
-            //to_file.Close();
-            //StreamReader from_file = new StreamReader("Gamers.txt");
-            //string text = from_file.ReadToEnd();
-            //Console.WriteLine(text);
-            //from_file.Close();
+                                snake.Draw();
+
+                                //создание точки "еда"
+                                FoodCreator foodCreator = new FoodCreator(80, 25, '*');
+                                Point food = foodCreator.CreateFood();
+                                food.Draw();
+
+                                //процесс игры
+                                while (true)
+                                {
+                                    //если фигура "стена" и фигура "хвост змеи" касается фигуры "змейка" игра прекращается
+                                    if (walls.IsHit(snake) || snake.IsHitTail())
+                                    {
+                                        break;
+                                    }
+                                    //если змея ест еду очки увеличиваются
+                                    if (snake.Eat(food))
+                                    {
+                                        food = foodCreator.CreateFood();
+                                        food.Draw();
+                                        score++;
+                                    }
+                                    //если события не происходят, то фигура "змейка" движется
+                                    else
+                                    {
+                                        snake.Move();
+                                    }
+
+                                    Thread.Sleep(100);
+                                    if (Console.KeyAvailable)
+                                    {
+                                        ConsoleKeyInfo key = Console.ReadKey();
+                                        snake.HandleKey(key.Key);
+                                    }
+                                }
+                                WriteGameOver(score, name);
+                                if (score >= 10)
+                                {
+                                    StreamWriter to_file = new StreamWriter("Gamers.txt", true);
+                                    to_file.WriteLine(name + " - " + score);
+                                    to_file.Close();
+                                }                                
+                                Thread.Sleep(2000);
+                                Console.Clear();
+                                break;
+                            case 1:
+                                Console.Clear();
+                                Console.WriteLine($"Edgar Neverovski TARpv21");
+                                Thread.Sleep(2000);
+                                Console.Clear();
+                                break;
+                            case 2:
+                                Console.Clear();
+                                StreamReader from_file = new StreamReader("Gamers.txt");
+                                string text = from_file.ReadToEnd();
+                                Console.WriteLine(text);
+                                from_file.Close();
+                                Thread.Sleep(2000);
+                                Console.Clear();
+                                break;
+                            case 3:
+                                Console.WriteLine("Выбран выход из приложения");
+                                return;
+                        }
+                        break;
+                }
+            }
 
             Console.ReadLine();
 		}
@@ -89,10 +146,11 @@ namespace Snake
 			Console.SetCursorPosition(xOffset, yOffset++);
 			WriteText("=====================", xOffset, yOffset++);
 			WriteText(" G A M E    O V E R ", xOffset, yOffset++);
-			WriteText($" {name}, your score: {score}", xOffset, yOffset++);
+			WriteText($"{name}, your score: {score}", xOffset, yOffset++);
 			WriteText("=====================", xOffset, yOffset++);
 		}
-		static void WriteText(String text, int xOffset, int yOffset)
+        //функция вывода текста
+        static void WriteText(String text, int xOffset, int yOffset)
 		{
 			Console.SetCursorPosition(xOffset, yOffset);
 			Console.WriteLine(text);
